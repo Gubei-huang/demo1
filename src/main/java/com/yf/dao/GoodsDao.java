fe7aff5c1fc11050f2491652ee7999269084d7a4
Huang <1305076494@qq.com>
2023-03-20 14:26:07 +0800

    Initial commit

---
+++ src/main/java/com/yf/dao/GoodsDao.java
@@ -0,0 +1,4 @@
+package com.yf.dao;
+
+public class GoodsDao {
+}
---
+++ src/main/java/com/yf/pojo/Goods.java
@@ -0,0 +1,10 @@
+package com.yf.pojo;
+
+public class Goods {
+    private long id;
+    private String name;
+    private String specification;
+    private String category;
+    private String description;
+
+}
